a81e11b18b55680ac1a07c3a9f89026a34fa7b3a
20danik <90417495+20danik@users.noreply.github.com>
2021-10-11 19:03:49 -0400

    fixed Playlist.java

--- src/main/java/Entities/Playlist.java
+++ src/main/java/Entities/Playlist.java
@@ -1,94 +1,70 @@
-package Entities;
 import java.util.*;
 
-public class Song{
-    private String name;
-    private String artist;
-    private int duration;
-    private String album;
-    private String genre;
-    private boolean explicit;
-    private int popularity;
+public class Playlist{
+    private List<Song> playlist;
+    private String playlistName;
 
     /**
-     * Construct a Song object, with the following parameters:
+     * Construct a Playlist object, with the following parameters:
      *
-     * @param name Name of the song
-     * @param artist Artist name
-     * @param duration How long the song is in seconds
-     * @param album Name of the album, might be a single
-     * @param genre Genre of song
-     * @param explicit Boolean representing whether the song is family friendly or not
-     * @param popularity Int representing spot in charts
-     */
-
-    //Song within an album
-
-    /**
-     * Constructs a song which has an associated album.
+     * @param playlistName Name of the playlist, may be unnamed
      *
      */
-    Song(String name, String artist, int duration, String album, String genre, boolean explicit, int popularity){
-        this.name = name;
-        this.artist = artist;
-        this.duration = duration;
-        this.album = album;
-        this.genre = genre;
-        this.explicit = explicit;
-        this.popularity = popularity;
+    //constructor
+    Playlist(){
+        this.playlist = new ArrayList<Song>();
+        this.playlistName = "Unnamed Playlist";
     }
 
-    /**
-     * Constructs a song that is a single (no associated album)
-     */
-    Song(String name, String artist, int duration, String genre, boolean explicit, int popularity){
-        this.name = name;
-        this.artist = artist;
-        this.duration = duration;
-        this.album = name;
-        this.genre = genre;
-        this.explicit = explicit;
-        this.popularity = popularity;
+    Playlist(String name){
+        this.playlist = new ArrayList<Song>();
+        this.playlistName = name;
     }
 
-    public String getName(){
-        return name;
+    public Song getSongAtIndex(int i){
+        if(i < this.getPlaylistLength()){
+            playlist.get(i);
+        }
     }
 
-    public int getDuration(){
-        return duration;
+    public String getPlaylistName(){
+        return playlistName;
     }
 
-    public String getArtist(){
-        // retruns artist name
-        return artist;
+    public String setPlaylistName(String newName){
+        //change playlist name and return a string with the new name
+        String oldName = this.getPlaylistName();
+        this.playlistName = newName;
+        return "Playlist " + oldName + " was renamed to: " + newName;
     }
 
-    public String getAlbum(){
-        //returns album from where the song is from
-        return album;
+    public int getPlaylistLength(){
+        // return length of playlist in terms of how many songs are in it
+        return playlist.size();
     }
 
-    public String getGenre(){
-        //returns genre of album
-        return genre;
+    @Override
+    public int compareTo(Playlist other){
+        //compare playlists by lengths? Maybe later we can do some funky compareTo with like how "popular" it is
+        if(this.getPlaylistLength() > other.getPlaylistLength()){
+            return 1;
+        }
+        else if(this.getPlaylistLength() < other.getPlaylistLength()){
+            return -1;
+        }
+        else{
+            //equal lengths
+            return 0;
+        }
     }
 
-    public boolean isExplicit(){
-        //returns True if song is explicit
-        return explicit;
-    }
-
-    public double getPopularity(){
-        //returns ranking on charts
-        return popularity;
-    }
-
-    /**
-     @Override toString
-     return song as a string in the form of: *Name*, *album name*, by *artist name*.
-     **/
+    @Override
     public String toString(){
-        return name + ", " + album + ", by: " + artist + ".";
+        //return string representation of playlist, just print string reps of songs in order
+        String s = "";
+        for(int i=0; i<this.getPlaylistLength(); i++){
+            s = i + ") " + this.getSongAtIndex(i).toString() + "\n";
+        }
+        return s;
     }
 }
