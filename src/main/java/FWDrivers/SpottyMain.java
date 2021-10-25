43f8127239510e368aae39c35bb788b80536c8ed
maplemochi <90563735+maplemochi@users.noreply.github.com>
2021-10-25 15:57:46 -0400

    Update SpottyMain.java

--- src/main/java/FWDrivers/SpottyMain.java
+++ src/main/java/FWDrivers/SpottyMain.java
@@ -7,11 +7,13 @@ import java.util.Scanner;
 
 public class SpottyMain {
     public static void main(String[] args) {
+        // Start a new appstate to keep track of the program current running state.
         AppState state = new AppState();
+        // Start a scanner to look for user inputs
         Scanner input = new Scanner(System.in);
 
         while (state.isRunning()){
-
+            // Render the homescreen text.
             System.out.println(state.renderHome());
             int numIn = input.nextInt();
             //TODO: Change the placeholders to start main methods for handling the displaying and user inputs for the menu options stated.
