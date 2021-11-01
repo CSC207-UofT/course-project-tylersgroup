9abd1668adf2cc28eabe74f942ab3a1ec010792f
Max <max.bridgewater@mail.utoronto.ca>
2021-10-31 22:39:20 -0400

    Converted to a web app

---
+++ src/test/java/com/example/spottywebapp/SpottyWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spottywebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpottyWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
