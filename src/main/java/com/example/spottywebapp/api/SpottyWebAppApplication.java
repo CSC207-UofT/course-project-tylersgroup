662c300c4eaa4aca17badb8a8d68549b8aa502f6
June Oh <JuneOh710@gmail.com>
2021-11-03 21:27:28 -0400

    before pull

--- src/main/java/com/example/spottywebapp/api/SpottyWebAppApplication.java
+++ src/main/java/com/example/spottywebapp/api/SpottyWebAppApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpottyWebAppApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SpottyWebAppApplication.class, args);
     }
-
 }
