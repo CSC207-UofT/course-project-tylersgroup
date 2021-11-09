469277b97cd644f0aa3baae9a9361a89d6f8c6cf
20danik <90417495+20danik@users.noreply.github.com>
2021-11-09 16:44:03 -0500

    Changes made to SpotifyApiController, added more mappings that are playlist specific. Hypothetically if authentication succeeds we should be able to use these commands to access the api.

--- src/main/java/com/example/spottywebapp/api/controller/SpotifyApiController.java
+++ src/main/java/com/example/spottywebapp/api/controller/SpotifyApiController.java
@@ -1,16 +1,18 @@
 package com.example.spottywebapp.api.controller;
 
 import com.wrapper.spotify.exceptions.SpotifyWebApiException;
-//import com.sun.el.parser.ParseException;
+import com.wrapper.spotify.model_objects.special.SnapshotResult;
 import com.wrapper.spotify.model_objects.specification.*;
 import com.wrapper.spotify.requests.data.personalization.simplified.GetUsersTopTracksRequest;
-import com.wrapper.spotify.requests.data.playlists.CreatePlaylistRequest;
-import com.wrapper.spotify.requests.data.playlists.GetListOfUsersPlaylistsRequest;
+import com.wrapper.spotify.requests.data.playlists.*;
 import com.wrapper.spotify.requests.data.users_profile.GetCurrentUsersProfileRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonParser;
+import org.apache.hc.core5.http.ParseException;
 
 import static com.example.spottywebapp.api.controller.SpotifyAuthController.spotifyApi;
 import java.io.IOException;
@@ -40,11 +42,11 @@ public class SpotifyApiController {
         return new Track[0];
     }
 
-    /**
-    //@GetMapping(value = "user-playlists")
-    public PlaylistSimplified[] getUserPlaylists() {
+    @GetMapping(value = "user-playlists")
+    public PlaylistSimplified[] getCurrentUserPlaylists(String user_id) {
 
-        final GetListOfUsersPlaylistsRequest getListOfUsersPlaylistsRequestRequest = spotifyApi.getListOfUsersPlaylists();
+        final GetListOfCurrentUsersPlaylistsRequest getListOfUsersPlaylistsRequestRequest = spotifyApi.getListOfCurrentUsersPlaylists()
+                .build();
 
         try {
 
@@ -58,11 +60,107 @@ public class SpotifyApiController {
         return new PlaylistSimplified[0];
     }
 
-    //@PostMapping(value = "create-playlist")
-    public PlaylistSimplified createNewPlaylist(String user_id, String name){
-        final CreatePlaylistRequest createNewPlaylistRequest = spotifyApi.createPlaylist(user_id, name);
+    @PostMapping(value = "create-playlist")
+    public void createNewPlaylist(String userId, String name){
+        final CreatePlaylistRequest createPlaylistRequest = spotifyApi.createPlaylist(userId, name)
+                .build();
+        try {
+            final Playlist playlist = createPlaylistRequest.execute();
+
+            System.out.println("Name: " + playlist.getName());
+        } catch (IOException | SpotifyWebApiException | ParseException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 
+    @PostMapping(value = "change-playlist-details")
+    public void changePlaylistDetails(String playlistId, String newName){
+        final ChangePlaylistsDetailsRequest changePlaylistsDetailsRequest = spotifyApi.changePlaylistsDetails(playlistId)
+                .name(newName)
+                .build();
+
+        try {
+            final String string = changePlaylistsDetailsRequest.execute();
+
+            System.out.println("Null: " + string);
+        } catch (IOException | SpotifyWebApiException | ParseException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+
+    }
+
+    //can probably make a new method that gets you the playlist by playlist name but have this for the time being
+    @GetMapping(value = "get-playlist")
+    public void getPlaylist(String playlistId){
+        final GetPlaylistRequest getPlaylistRequest = spotifyApi.getPlaylist(playlistId)
+                .build();
+
+        try {
+            final Playlist playlist = getPlaylistRequest.execute();
+
+            System.out.println("Name: " + playlist.getName());
+        } catch (IOException | SpotifyWebApiException | ParseException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    @GetMapping(value = "get-playlist-items")
+    public void getPlaylistItems(String playlistId){
+        final GetPlaylistsItemsRequest getPlaylistsItemsRequest = spotifyApi.getPlaylistsItems(playlistId)
+                .build();
+        // can also add limit
+        try {
+            final Paging<PlaylistTrack> playlistTrackPaging = getPlaylistsItemsRequest.execute();
+
+            System.out.println("Total: " + playlistTrackPaging.getTotal());
+        } catch (IOException | SpotifyWebApiException | ParseException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    @PostMapping(value = "remove-items")
+    public void removeItemsFromPlaylist(String playlistId, JsonArray tracks) {
+        final RemoveItemsFromPlaylistRequest removeItemsFromPlaylistRequest = spotifyApi
+                .removeItemsFromPlaylist(playlistId, tracks)
+                .build();
+        try {
+            final SnapshotResult snapshotResult = removeItemsFromPlaylistRequest.execute();
+
+            System.out.println("Snapshot ID: " + snapshotResult.getSnapshotId());
+        } catch (IOException | SpotifyWebApiException | ParseException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    @PostMapping(value = "add-items-to-playlist")
+    public void addItemsToPlaylist(String playlist_id, String[] uris){
+        final AddItemsToPlaylistRequest addItemsToPlaylistRequest = spotifyApi.addItemsToPlaylist(playlist_id, uris)
+                .build();
+
+        try {
+            final SnapshotResult snapshotResult = addItemsToPlaylistRequest.execute();
+
+            System.out.println("Snapshot ID: " + snapshotResult.getSnapshotId());
+        } catch (IOException | SpotifyWebApiException | ParseException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    @PostMapping(value = "add-items-to-playlist-position")
+    public void addItemsToPlaylist(String playlist_id, String[] uris, int position){
+        final AddItemsToPlaylistRequest addItemsToPlaylistRequest = spotifyApi.addItemsToPlaylist(playlist_id, uris)
+                .position(position)
+                .build();
+
+        try {
+            final SnapshotResult snapshotResult = addItemsToPlaylistRequest.execute();
+
+            System.out.println("Snapshot ID: " + snapshotResult.getSnapshotId());
+        } catch (IOException | SpotifyWebApiException | ParseException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
 
     public static void getCurrentUsersProfile_Sync() {
         final GetCurrentUsersProfileRequest getCurrentUsersProfileRequest = spotifyApi.getCurrentUsersProfile()
@@ -71,9 +169,8 @@ public class SpotifyApiController {
             final User user = getCurrentUsersProfileRequest.execute();
 
             System.out.println("Display name: " + user.getDisplayName());
-        } catch (IOException | SpotifyWebApiException | ParseException e) { //TODO: Fix ParseException not importing
+        } catch (IOException | SpotifyWebApiException | ParseException e) {
             System.out.println("Error: " + e);
         }
     }
-    **/
 }
