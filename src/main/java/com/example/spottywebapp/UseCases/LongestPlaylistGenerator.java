3f28bb9c00fcf53322b5e28530fa03579cc48d74
20danik <90417495+20danik@users.noreply.github.com>
2021-11-13 13:29:12 -0500

    Edited LongestPlaylistGenerator and ShortestPlaylistGenerator, adding methods generatePlaylistWeb in order for the commandline interface to still work.

--- src/main/java/com/example/spottywebapp/UseCases/LongestPlaylistGenerator.java
+++ src/main/java/com/example/spottywebapp/UseCases/LongestPlaylistGenerator.java
@@ -9,6 +9,15 @@ import java.util.List;
 public class LongestPlaylistGenerator implements PlaylistGenerationStrategy{
     public Playlist generatePlaylist(ArrayList<String> words, Playlist newPlaylist){
         for (String s : words) {
+            Song song = SongGenerator.generateSong(s); //create a song entity for that song
+            // receive the song entities and instantiate a playlist entity
+            newPlaylist.addSong(song);
+        }
+        return newPlaylist;
+    }
+
+    public Playlist generatePlaylistWeb(ArrayList<String> words, Playlist newPlaylist){
+        for (String s : words) {
             Song song = SongGenerator.generateSongSpotify(s); //create a song entity for that song
             // receive the song entities and instantiate a playlist entity
             newPlaylist.addSong(song);
