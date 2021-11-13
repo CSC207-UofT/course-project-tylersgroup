230161d6b1a5b45bdb702eac30a8ec6109116e2a
TylerGit <tylercy.yan@mail.utoronto.ca>
2021-11-13 15:23:02 -0500

    All testing passed for PlaylistManager and UserManager

--- src/test/java/UseCases/UserManagerTest.java
+++ src/test/java/UseCases/UserManagerTest.java
@@ -9,41 +9,39 @@ import org.junit.jupiter.api.Test;
 
 
 public class UserManagerTest {
-    UserManager manager;
+    UserManager manager = UserManager.getInstance();
 
     @Before("")
     public void setUp(){
-        manager = new UserManager();
     }
 
     @After("")
     public void tearDown(){
-
     }
 
     @Test
-    public void testAddUserSuccess(){
-        boolean success = manager.addUser("TestUser1","User1Password");
+    public void testCreateAndAddUserSuccess(){
+        boolean success = manager.createAndAddUser("TestUser1","User1Password");
         assert(success);
     }
 
     @Test
-    public void testAddUserFailure(){
-        manager.addUser("TestUser1","User1Password");
-        boolean success = manager.addUser("TestUser1","UpdatingMyPassword");
+    public void testCreateAndAddUserFailure(){
+        manager.createAndAddUser("TestUser1","User1Password");
+        boolean success = manager.createAndAddUser("TestUser1","UpdatingMyPassword");
         assert(!success);
     }
 
     @Test
     public void testDeleteUserSuccess(){
-        manager.addUser("TestUser1","User1Password");
+        manager.createAndAddUser("TestUser1","User1Password");
         boolean success = manager.deleteUser("TestUser1");
         assert(success);
     }
 
     @Test
     public void testDeleteUserFailure(){
-        boolean success = manager.deleteUser("TestUser1");
+        boolean success = manager.deleteUser("TestUser10");
         assert(!success);
     }
 
