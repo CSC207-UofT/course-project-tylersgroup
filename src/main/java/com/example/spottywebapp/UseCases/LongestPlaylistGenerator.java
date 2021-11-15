8163aa8e5cca142e78c7cb8902df33512d297346
June Oh <82453596+JuneOh710@users.noreply.github.com>
2021-11-15 08:27:15 -0500

    Update LongestPlaylistGenerator.java

--- src/main/java/com/example/spottywebapp/UseCases/LongestPlaylistGenerator.java
+++ src/main/java/com/example/spottywebapp/UseCases/LongestPlaylistGenerator.java
@@ -17,26 +17,26 @@ public class LongestPlaylistGenerator implements PlaylistGenerationStrategy{
     }
 
     public Playlist generatePlaylistWeb(ArrayList<String> words, Playlist newPlaylist){
-//        for (String s : words) {
-//            Song song = SongGenerator.generateSongSpotify(s); //create a song entity for that song
-//            // receive the song entities and instantiate a playlist entity
-//            newPlaylist.addSong(song);
-//        }
-        String s;
-        for (int i=0; i < words.size(); i +=2) {
-            if (words.size() - i == 3 ) {
-                s = words.subList(i, words.size()).toString();
-                Song song = SongGenerator.generateSongSpotify(s); //create a song entity for that song
-                // receive the song entities and instantiate a playlist entity
-                newPlaylist.addSong(song);
-                return newPlaylist;
-            } else {
-                s = words.subList(i, i + 2).toString();
-            }
-            Song song = SongGenerator.generateSongSpotify(s); //create a song entity for that song
-            // receive the song entities and instantiate a playlist entity
-            newPlaylist.addSong(song);
-        }
+       for (String s : words) {
+           Song song = SongGenerator.generateSongSpotify(s); //create a song entity for that song
+           // receive the song entities and instantiate a playlist entity
+           newPlaylist.addSong(song);
+       }
+//         String s;
+//         for (int i=0; i < words.size(); i +=2) {
+//             if (words.size() - i == 3 ) {
+//                 s = words.subList(i, words.size()).toString();
+//                 Song song = SongGenerator.generateSongSpotify(s); //create a song entity for that song
+//                 // receive the song entities and instantiate a playlist entity
+//                 newPlaylist.addSong(song);
+//                 return newPlaylist;
+//             } else {
+//                 s = words.subList(i, i + 2).toString();
+//             }
+//             Song song = SongGenerator.generateSongSpotify(s); //create a song entity for that song
+//             // receive the song entities and instantiate a playlist entity
+//             newPlaylist.addSong(song);
+//         }
         return newPlaylist;
     }
 }
