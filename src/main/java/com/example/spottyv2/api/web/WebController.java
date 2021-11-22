d2a538ad31b69ba7b8563e67bb47f017df1dabf3
maplemochi <max.bridgewater@mail.utoronto.ca>
2021-11-22 16:07:51 -0500

    Checking git commits.

--- src/main/java/com/example/spottyv2/api/web/WebController.java
+++ src/main/java/com/example/spottyv2/api/web/WebController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+//TODO: Add javadoc!
+
 @Controller
 public class WebController {
 
