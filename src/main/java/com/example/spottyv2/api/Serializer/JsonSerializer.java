76eb0d71f964567a5c4fa6c26a2c23766ba37181
Minori <66072052+min-chin@users.noreply.github.com>
2021-11-25 19:31:52 -0500

    Json serialization adn deserialization

--- src/main/java/com/example/spottyv2/api/Serializer/JsonSerializer.java
+++ src/main/java/com/example/spottyv2/api/Serializer/JsonSerializer.java
@@ -1,17 +1,20 @@
 package com.example.spottyv2.api.Serializer;
 
 import com.example.spottyv2.Entities.Playlist;
-//import com.github.cliftonlabs.json_simple.JsonArray;
-//import com.github.cliftonlabs.json_simple.JsonObject;
-//import com.github.cliftonlabs.json_simple.Jsonable;
+import com.github.cliftonlabs.json_simple.JsonArray;
+import com.github.cliftonlabs.json_simple.JsonObject;
+import com.github.cliftonlabs.json_simple.Jsonable;
 import com.github.cliftonlabs.json_simple.Jsoner;
-//import com.github.dozermapper
+import com.github.dozermapper.core.DozerBeanMapperBuilder;
+import com.github.dozermapper.core.Mapper;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class JsonSerializer {
+
 
 //    public Playlist createjson(Playlist playlist) {
 //        try {
@@ -40,34 +43,22 @@ public class JsonSerializer {
     }
 
 
-//    public Playlist deserialize() {
-//        Playlist playlist = new Playlist();
-//        try {
-//            Reader reader = Files.newBufferedReader(Paths.get("playlists.json"))
-//            JsonArray objects = Jsoner.deserializeMany(reader);
-//            Mapper mapper = DozerBeanMapperBuilder.buildDefault();
-//            // Mapper through spring?
-//            JsonArray array = (JsonArray) playlists.get(0);
-//            ArrayList<Playlist> playlists = array.stream()
-//                    .map(obj -> mapper.map(obj, Playlist.class))
-//                    .collect(Collectors.toList());
-//            // add a return statement? pass to another class
-//            reader.close();
-//        } catch (Exception ex) {
-//            ex.printStackTrace();
-//        }
-//   }
-
-//    public ArrayList<Playlist> getPlaylists() {
-//        ArrayList<Playlist> playlists = new ArrayList<>();
-//        File folder = new File("/Users/maxbridgewater/IdeaProjects/course-project-tylersgroup1/SerializedPlaylists");
-//        File[] listOfFiles = folder.listFiles();
-//        assert listOfFiles != null;
-//        for (File listOfFile : listOfFiles) {
-//            if (listOfFile.isFile()) {
-//                playlists.add(deserialize(listOfFile.getName()));
-//            }
-//        }
-//        return playlists;
-//    }
+    public ArrayList<Playlist> getPlaylist() {
+        try {
+            Reader reader = Files.newBufferedReader(Paths.get("playlists.json"));
+            JsonArray objects = Jsoner.deserializeMany(reader);
+            Mapper mapper = DozerBeanMapperBuilder.buildDefault();
+            // Mapper through spring?
+            JsonArray array = (JsonArray) objects.get(0);
+            ArrayList<Playlist> playlists = (ArrayList<Playlist>) array.stream()
+                    .map(obj -> mapper.map(obj, Playlist.class))
+                    .collect(Collectors.toList());
+            // add a return statement? pass to another class
+            reader.close();
+            return playlists;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return null;
+   }
 }
