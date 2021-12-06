afaae000cab3f30e9b4fb61891bdc5eaa0cd49fc
maplemochi <max.bridgewater@mail.utoronto.ca>
2021-12-06 15:40:36 -0500

    Small tweaks to remove outdated methods.

--- src/main/java/com/example/spottyv2/api/web/WebManageController.java
+++ src/main/java/com/example/spottyv2/api/web/WebManageController.java
@@ -20,56 +20,6 @@ import java.util.*;
 public class WebManageController {
 
 
-//    @RequestMapping(value = {"/manage","/manage?id={id}",  "/manage/{id}"}, method = RequestMethod.GET)
-//
-//    public ModelAndView manage(@PathVariable(required = false) String id, @RequestParam String id2) {
-//
-//        ModelAndView modelAndView = new ModelAndView("manage");
-//        System.out.println("Id: " + id);
-//        System.out.println("ID2: " + id2);
-//
-//
-//        //TODO: This needs to be moved to either a helper method or use case class. (For clean architecture)
-//        JsonSerializer getter = new JsonSerializer();
-//        //Serializer getter = new Serializer();
-//        HashMap<String, String> out = new HashMap<>();
-//        ArrayList<Playlist> playlists = getter.getPlaylists(getter.loggedInUserInfo(id));
-//        //ArrayList<Playlist> playlists = getter.getPlaylists();
-//        List<String> headers = Arrays.asList("Playlist Name", "Playlist", "Add to your spotify", "Get shareable link");
-//        List<Map<String, Object>> rows = new ArrayList<>();
-//        //String userID = "alphaturtle3621";
-//
-////        for(Playlist playlist : playlists){
-////            out.put(playlist.getPlaylistName(), playlist.toString());
-////
-////            rows.add(Map.of("Playlist Name", playlist.getPlaylistName(), "Playlist", playlist.toString(),
-////                    "Add to your spotify",
-////
-////                    "<form>" + "<input type=\"hidden\" id=\"id\" name=\"id\"" + " value=\"" + "Alphaturtle3621"
-////                            + "\" "+
-////                            "<input type=\"hidden\" id=\"name\" name=\"name\"" + " value=\"" + playlist.getPlaylistName()
-////                            + "\" "+
-////                            "<input type=\"hidden\" id=\"playlist\" name=\"playlist\"" + " value=\"" + "${"
-////                            + "\" "+
-////                            "</form>"
-////
-////                    , "Get shareable link", "N/A"));
-////        }
-//        //modelAndView.addObject("playlistMap", out);
-//
-//
-//        modelAndView.addObject("headers", headers);
-//        modelAndView.addObject("playlistList", playlists);
-//        modelAndView.addObject("userID", id);
-//        //modelAndView.addObject("rows", rows);
-//
-//        //Console logging for debugging
-//        System.out.println("All playlists:\n" + out);
-//
-//        return modelAndView;
-//
-//    }
-
     @GetMapping(path = {"/manage", "/manage/{data}"})
     public ModelAndView user(@PathVariable(required=false,name="data") String data,
                      @RequestParam(required=false) Map<String,String> qparams) {
